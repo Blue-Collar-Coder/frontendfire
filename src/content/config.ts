ccc7e5be5761f79077f27185bb0f3f587e62e4c7
TJ VanToll <tj.vantoll@gmail.com>
2024-03-03 22:05:36 -0500

    Data for the first four episodes

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,13 +4,19 @@ const episodes = defineCollection({
   type: "data",
   schema: z.object({
     number: z.number(),
-    tags: z.array(z.enum(["news", "interview"])),
+    type: z.enum(["news", "interview"]),
     title: z.string(),
     date: z.date(),
     description: z.string(),
     duration: z.string(),
     hosts: z.array(z.string()),
     guests: z.array(z.string()).optional(),
+    links: z.array(
+      z.object({
+        text: z.string(),
+        url: z.string(),
+      })
+    ),
     spotify: z.string(),
     clip: z.string(),
     youtube: z.string(),
